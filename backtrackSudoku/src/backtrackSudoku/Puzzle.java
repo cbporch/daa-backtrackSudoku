e3ea7d9815160bd35ae6e2da9a394eb1998a0701
grillo88 <grillo88@RobiLab303-22.rowanads.rowan.edu>
2015-10-14 12:10:53 -0400

    Signed-off-by: grillo88 <grillo88@RobiLab303-22.rowanads.rowan.edu>

--- backtrackSudoku/src/backtrackSudoku/Puzzle.java
+++ backtrackSudoku/src/backtrackSudoku/Puzzle.java
@@ -32,7 +32,33 @@ public class Puzzle {
 		}
 		return col;
 	}
-
+	
+	public int[] getBlock(int r, int c){
+		int[] block = new int[size];
+		int row = 0,col = 0;
+		for (int i=0; i <= c; i++){
+			if(i%y == 0)
+			{
+				col++;
+			}
+		}
+		for (int i=0; i <= r; i++){
+			if(i%x == 0){
+				row++;
+			}
+		}
+		int x_offset = x*(row-1);
+		int y_offset = y*(col-1);
+		int count = 0;
+		for (int j = x_offset; j < (x + x_offset); j++) {
+			for (int k = y_offset; k < y + y_offset; k++) {
+				block[count] = sdk[j][k];
+				count++;
+			}
+		}
+		return block;
+	}
+	
 	public int getSize() {
 		return size;
 	}
@@ -47,7 +73,5 @@ public class Puzzle {
 
 	public int[][] getGrid() {
 		return sdk;
-	}
-	
-	
+	}	
 }
