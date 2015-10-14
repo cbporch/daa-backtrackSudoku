ee0617c48dec1731820d400c2eb4867163f3e9a2
Christopher Porch <porchc0@students.rowan.edu>
2015-10-14 10:35:54 -0400

    Worked on Cell.reduce
    Signed-off-by: Christopher Porch <porchc0@students.rowan.edu>

--- backtrackSudoku/src/backtrackSudoku/Cell.java
+++ backtrackSudoku/src/backtrackSudoku/Cell.java
@@ -4,12 +4,12 @@ public class Cell {
 	
 	int x;
 	int y;
-	Queue<Integer> options[];
+	ABQueue<Integer> options;
 	
 	public Cell(int x, int y, int size) {
 		this.x = x;
 		this.y = y;
-		options = new Queue[size];
+		options = new ABQueue<Integer>(size);
 	}
 	
 	public int getX() {
@@ -20,7 +20,7 @@ public class Cell {
 		return y;
 	}
 	
-	public Queue<Integer>[] getOptions(){
+	public ABQueue<Integer> getOptions(){
 		return options;
 	}
 	
@@ -28,6 +28,35 @@ public class Cell {
 	 * For a given puzzle, reduce available options based on location
 	 */
 	public void reduceOptions(Puzzle p){
-		
+		int temp[] = p.getRow(x);
+		for(int i = 0; i < temp.length; i++){
+			int a = temp[i];
+			int l = options.length();
+			for(int j = 0; j < l; j++){
+				if(a == options.peek()){
+					options.dequeue();
+				}
+			}
+		}
+		temp = p.getColumn(y);
+		for(int i = 0; i < temp.length; i++){
+			int a = temp[i];
+			int l = options.length();
+			for(int j = 0; j < l; j++){
+				if(a == options.peek()){
+					options.dequeue();
+				}
+			}
+		}
+		//temp = p.getBlock(x,y);
+		//for(int i = 0; i < temp.length; i++){
+		//	int a = temp[i];
+		//	int l = options.length();
+		//	for(int j = 0; j < l; j++){
+		//		if(a == options.peek()){
+		//			options.dequeue();
+		//		}
+		//	}
+		//}
 	}
 }
